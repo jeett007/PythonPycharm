4f5388ffb4e8803162430e910ca9e792a7faa4e5
ARIJIT GHOSH <arijitghosh2306@gmail.com>
2025-07-18 15:13:12 +0530

    by Arijit Ghosh

---
+++ Greeting.py
@@ -0,0 +1,11 @@
+# Program to greet the user using their full name
+
+# Taking input from the user
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenating first name and last name
+full_name = first_name + " " + last_name
+
+# Displaying the personalized greeting
+print("Hello, " + full_name + "! Welcome!")
