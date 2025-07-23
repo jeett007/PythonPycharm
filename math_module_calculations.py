d622993cc813868ac8965f01e7c36bb616b95b57
ARIJIT GHOSH <arijitghosh2306@gmail.com>
2025-07-23 21:23:52 +0530

    by Arijit Ghosh

---
+++ math_module_calculations.py
@@ -0,0 +1,11 @@
+import math
+
+num = float(input("Enter a number: "))
+
+sqrt_val = math.sqrt(num)
+log_val = math.log(num)
+sine_val = math.sin(num)
+
+print(f"Square Root of {num} is: {sqrt_val}")
+print(f"Natural Logarithm of {num} is: {log_val}")
+print(f"Sine of {num} radians is: {sine_val}")
